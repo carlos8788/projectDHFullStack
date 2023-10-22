5aa9b3574482ee9fba1790f7b6d10c3eeb1f5161
carlos8788 <carlos8788@gmail.com>
2023-10-22 10:48:26 -0300

    product route and form create product in process

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/productController')
 const {authenticate} = require('../middlewares/authenticate')
-
+const upload = require('../middlewares/uploadImg.js')
 
 router.get('/productDetail/:id', controller.productDetail)
-router.get('/formCreateProduct', authenticate, controller.formCreateProduct)
+router.get('/formCreateProduct', authenticate, upload.single('productImg'), controller.formCreateProduct)
 router.post('/createProduct', controller.createProduct)
 router.get('/products', controller.products)
 
