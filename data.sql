5287ae96a0096e6d62632cdb939d6998c3d2c38a
carlos8788 <carlos8788@gmail.com>
2023-11-07 09:40:22 -0300

    data and images

---
+++ data.sql
@@ -0,0 +1,32 @@
+-- Poblar la tabla color
+INSERT INTO `river_shopping`.`color` (name) VALUES 
+('violeta'), 
+('rojo'), 
+('negro'), 
+('blanco'), 
+('gris');
+
+-- Poblar talles
+INSERT INTO `river_shopping`.`size` (`name`) VALUES 
+('sm'), 
+('s'), 
+('m'), 
+('l'), 
+('xl'), 
+('xxl');
+
+
+INSERT INTO `river_shopping`.`category` (`name`) VALUES 
+('camiseta'), 
+('campera'), 
+('varios'), 
+('pelota'), 
+('botines'), 
+('pantalones'), 
+('medias');
+
+INSERT INTO `river_shopping`.`brand` (`name`) VALUES 
+('adidas'), 
+('generico'), 
+('river plate');
+
